cff86969a0a07864fa4515166c2ba86feab9a587
Pavlo Havrylov <greenpasha22@gmail.com>
2020-01-29 11:41:40 +0200

    upgrade clien methods

--- client.py
+++ client.py
@@ -4,6 +4,7 @@ from json import dumps
 import logging
 import urllib3
 from config import port, url
+
 logging.basicConfig(filename='/Users/phavry/Desktop/logfile',
                     filemode='a',
                     format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
@@ -19,28 +20,40 @@ simplefilter(
 
 
 def post_data(text_message, queue=0, **kwargs):
-    param = dumps({"message": text_message, "queue": queue})
+    if queue == 0:
+        param = dumps({"message": text_message})
+    else:
+        param = dumps({"message": text_message, "queue": queue})
     response = requests.post(url=f'{url}:{port}',
                              json=param, **kwargs)
     return response
 
 
 def get_data(queue=0, **kwargs):
-    param = {'queue': queue}
+    if queue == 0:
+        param = {}
+    else:
+        param = {'queue': queue}
     response = requests.get(url=f'{url}:{port}',
                             params=param, **kwargs)
     return response
 
 
 def put_data(text_message, queue=0, **kwargs):
-    param = dumps({"message": text_message, "queue": queue})
+    if queue == 0:
+        param = dumps({"message": text_message})
+    else:
+        param = dumps({"message": text_message, "queue": queue})
     response = requests.put(url=f'{url}:{port}',
                             data=param, **kwargs)
     return response
 
 
 def delete_data(queue=0, **kwargs):
-    param = {'queue': queue}
+    if queue == 0:
+        param = {}
+    else:
+        param = {'queue': queue}
     response = requests.delete(url=f'{url}:{port}',
                                params=param, **kwargs)
     return response
